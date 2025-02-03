86b73ab16e14c269f7775b0d6daa3f966cbf9fbb
PressedK <166111473+PressedK@users.noreply.github.com>
2025-02-03 20:03:38 +0800

    Defined Fraction Constructor for String Inputs
    Note: Recommit due to initial commitment
    
    Previous Description:
    To-Do: Fraction Constructor for non-string inputs

--- Fraction.py
+++ Fraction.py
@@ -1,8 +1,38 @@
 class Fraction(object):
 
     def __init__(self, numerator=0, denominator=1):
-        #TODO
-        pass
+        
+        if isinstance(numerator, str):
+
+            termsList = numerator.strip().split("/")
+       
+            if len(termsList) == 1:
+                #numerator 
+                if not termsList[0].isdigit() or isinstance(termsList[0], float):
+                    self.numerator = 0
+                else:
+                    self.numerator = int(termsList[0])
+            
+            elif len(termsList) == 2:
+                #numerator
+                if not termsList[0].isdigit() or isinstance(termsList[0], float):
+                    self.numerator = 0
+                else:
+                    self.numerator = int(termsList[0])
+
+                #denominator
+                if not termsList[1].isdigit() or isinstance(termsList[1], float):
+                        self.denominator = 1
+                else:
+                    self.denominator = int(termsList[1]) 
+
+            else:
+                self.numerator = 0
+                self.denominator = 1
+
+        else:
+            #TODO
+            pass
 
     def gcd(a, b):
         #TODO
